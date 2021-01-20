e358a31eca2f29602a30a2721a598e0719a87dd3
박인호 <eloy@jober.io>
2021-01-20 23:52:34 +0900

    file-loader, image-loader webpack config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ module.exports = withAntdLess({
   cssLoaderOptions: {},
 
   webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000
+        }
+      }
+    });
+
     return config;
   },
 });
